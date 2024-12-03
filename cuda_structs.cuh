bb560a080c64039aeac42f783781ed7c85fff400
Dosalock <intesaft@gmail.com>
2024-12-03 15:30:53 +0100

    fixed clang-format file finally

--- cuda_structs.cuh
+++ cuda_structs.cuh
@@ -3,17 +3,22 @@
 #define __CUDACC__
 
 #include <cuda.h>
+#include <cuda_fp16.h>
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
-#include <cuda_fp16.h>
-
-
 
 struct float3x3
 {
-	float4 m00_m01_m02,
-		m10_m11_m12,
-		m20_m21_m22;
+    float4 m00_m01_m02;
+    float4 m10_m11_m12;
+    float4 m20_m21_m22;
+
+    float3x3 ( float4 m00_m01_m02, float4 m10_m11_m12, float4 m20_m21_m22 )
+    {
+        this->m00_m01_m02 = m00_m01_m02;
+        this->m10_m11_m12 = m10_m11_m12;
+        this->m20_m21_m22 = m20_m21_m22;
+    }
 };
 
 /**
@@ -23,121 +28,183 @@ struct float3x3
  * @param z - z-axis coordinate
  * @return float4{x,y,z,0}
  */
-inline __device__ float4 make_float4(const float &x, const float &y, const float &z)
+inline __device__ float4 make_float4 ( const float &x,
+                                       const float &y,
+                                       const float &z )
 {
-	return make_float4(x, y, z, 0);
+    return make_float4( x, y, z, 0 );
 }
 
-inline __device__ float4 operator*(const float &a, const float4 &b)
+/**
+ * @brief Overload multiplication to handle float ab
+ */
+inline __device__ float4 operator* ( const float &a, const float4 &b )
 {
-	return make_float4(a*b.x, a*b.y, a*b.z);
+    return make_float4( a * b.x, a * b.y, a * b.z );
 }
 
-inline __device__ float4 operator-(const float4 &a, const float4 &b)
+/**
+ * @brief Subtraction operator for proper float4 - float4 behaviour
+ */
+inline __device__ float4 operator- ( const float4 &a, const float4 &b )
 {
-	return make_float4(a.x-b.x,
-					   a.y-b.y,
-					   a.z-b.z);
+    return make_float4( a.x - b.x, a.y - b.y, a.z - b.z );
 }
 
-inline __device__ float4 operator+(const float4 &a, const float4 &b)
+/**
+ * @brief Addition operator for proper float4 - float4 behaviour
+ */
+inline __device__ float4 operator+ ( const float4 &a, const float4 &b )
 {
-	return make_float4(a.x+b.x,
-					   a.y+b.y,
-					   a.z+b.z);
+    return make_float4( a.x + b.x, a.y + b.y, a.z + b.z );
 }
 
-inline __device__ float dot(const float4 &a, const float4 &b)
+/**
+ * @brief Dot Multiplication, not only x,y,z are multiplied w is set to 0
+ * @param a - float4 vector
+ * @param b - float4 vector
+ * @return Dot multiplication of a and b
+ */
+inline __device__ float dot ( const float4 &a, const float4 &b )
 {
-	return a.x*b.x+a.y*b.y+a.z*b.z+0;
+    return a.x * b.x + a.y * b.y + a.z * b.z + 0;
 }
 
-inline __device__ float4 normalize(const float4 &v)
+/**
+ * @brief Normalize vector
+ * @param v - Vector to be normalized
+ * @return normalized vector of *v*
+ */
+inline __device__ float4 normalize ( const float4 &v )
 {
-	return __fsqrt_rd(dot(v, v))*v;
+    return __fsqrt_rd( dot( v, v ) ) * v;
 }
 
-inline __device__ float4 invert(const float4 &v)
+/**
+ * @brief Invert vector
+ * @param v - Vector to be inverted
+ * @return Inverted vector of *v*
+ */
+inline __device__ float4 invert ( const float4 &v )
 {
-	return make_float4(v.x*-1,
-					   v.y*-1,
-					   v.z*-1);
+    return make_float4( v.x * -1, v.y * -1, v.z * -1 );
 }
 
-inline __device__ double len(const float4 &v)
+/**
+ * @brief Returns length of vector
+ * @param v - Vector to be measured
+ * @return Length as double of vector *v*
+ */
+inline __device__ float len ( const float4 &v )
 {
-	return __fsqrt_rd(dot(v, v));
+    return __fsqrt_rd( dot( v, v ) );
 }
 
-inline __device__ float4 mul(const float4 &a, const float3x3 &b)
+/**
+ * @brief Matrix mutliplication of a vector and a matrix
+ * @param a - Vector to be multiplied
+ * @param b - Matrix to be multiplied
+ * @return Vector *a* multiplied with matrix *b*
+ */
+inline __device__ float4 mul ( const float4 &a, const float3x3 &b )
 {
-	return make_float4(dot(b.m00_m01_m02, a),
-					   dot(b.m10_m11_m12, a),
-					   dot(b.m20_m21_m22, a));
+    return make_float4( dot( b.m00_m01_m02, a ),
+                        dot( b.m10_m11_m12, a ),
+                        dot( b.m20_m21_m22, a ) );
 }
 
-inline __device__ float degrees_to_radians(const float &degrees)
+/**
+ * @brief Fast appoximate degrees to radian
+ * @param degrees - Degrees
+ * @return Radian conversion of *degrees*
+ */
+inline __device__ float degrees_to_radians ( const float &degrees )
 {
-	return 0.017453292*degrees; /* PI / 180 * degrees */
+    return 0.017453292 * degrees; /* PI / 180 * degrees */
 }
 
-inline __device__ float4 rotate_yaw(const float4 &a, const float &yaw)
+inline __device__ float4 rotate_yaw ( const float4 &a, const float &yaw )
 {
-	float rad = degrees_to_radians(yaw);
-	float cos_y = __cosf(rad);
-	float sin_y = __sinf(rad);
+    float rad   = degrees_to_radians( yaw );
+    float cos_y = __cosf( rad );
+    float sin_y = __sinf( rad );
 
-	return make_float4(a.x*cos_y+a.z*sin_y,
-					   a.y,
-					   -a.x*sin_y+a.z*cos_y);
+    return make_float4( a.x * cos_y + a.z * sin_y,
+                        a.y,
+                        -a.x * sin_y + a.z * cos_y );
 }
 
-inline __device__ float4 rotate_pitch(const float4 &a, const float pitch)
+inline __device__ float4 rotate_pitch ( const float4 &a, const float pitch )
 {
-	float rad = degrees_to_radians(pitch);
-	float cos_x = __cosf(rad);
-	float sin_x = __sinf(rad);
+    float rad   = degrees_to_radians( pitch );
+    float cos_x = __cosf( rad );
+    float sin_x = __sinf( rad );
 
-	return make_float4(a.x,
-					   a.y*cos_x-a.z*sin_x,
-					   a.y*sin_x+a.z*cos_x);
+    return make_float4( a.x,
+                        a.y * cos_x - a.z * sin_x,
+                        a.y * sin_x + a.z * cos_x );
 }
 
-inline __device__ float4 rotate_roll(const float4 &a, const float &roll)
+inline __device__ float4 rotate_roll ( const float4 &a, const float &roll )
 {
-	float rad = degrees_to_radians(roll);
-	float cos_z = __cosf(rad);
-	float sin_z = __sinf(rad);
+    float rad   = degrees_to_radians( roll );
+    float cos_z = __cosf( rad );
+    float sin_z = __sinf( rad );
 
-	return make_float4(a.x*cos_z-a.y*sin_z,
-					   a.x*sin_z+a.y*cos_z,
-					   a.z);
+    return make_float4( a.x * cos_z - a.y * sin_z,
+                        a.x * sin_z + a.y * cos_z,
+                        a.z );
 }
 
-inline __device__ float4 apply_camera_rotation(const float4 &cam,
-											   const float &yaw,
-											   const float &pitch,
-											   const float &roll)
+inline __device__ float4 apply_camera_rotation ( const float4 &cam,
+                                                 const float  &yaw,
+                                                 const float  &pitch,
+                                                 const float  &roll )
 {
-	float4 a = {};
-	a = rotate_yaw(cam, yaw);
-	a = rotate_pitch(cam, pitch);
-	a = rotate_roll(cam, roll);
+    float4 a = cam;
 
-	return a;
+
+    float4 rad = {
+        degrees_to_radians( yaw ),
+        degrees_to_radians( pitch ),
+        degrees_to_radians( roll ),
+    };
+
+
+    float3x3 x_rotation = {
+        { 1, 0,               0               },
+        { 0, __cosf( rad.x ), __sinf( rad.x ) },
+        { 0, __sinf( rad.x ), __cosf( rad.x ) },
+    };
+
+    float3x3 y_rotation = {
+        { __cosf( rad.y ), 0, -__sinf( rad.y ) },
+        { 0,               1, 0                },
+        { __sinf( rad.y ), 0, __cosf( rad.y )  },
+    };
+
+    float3x3 z_rotation = {
+        { __cosf( rad.z ), -__sinf( rad.z ), 0 },
+        { __sinf( rad.z ), __cosf( rad.z ),  0 },
+        { 0,               0,                1 },
+    };
+
+    a = mul( a, x_rotation );
+    a = mul( a, y_rotation );
+    a = mul( a, z_rotation );
+
+    return a;
 }
 
-inline __device__ float4 calc_forward_euler_angle(const float &yaw, const float &pitch)
+inline __device__ float4 calc_forward_euler_angle ( const float &yaw,
+                                                    const float &pitch )
 {
-	float r_pitch = degrees_to_radians(pitch);
-	float r_yaw = degrees_to_radians(yaw);
-	float cos_pitch = __cosf(r_pitch);
-	float sin_pitch = __sinf(r_pitch);
-	float cos_yaw = __cosf(r_yaw);
-	float sin_yaw = __sinf(r_yaw);
+    float r_pitch   = degrees_to_radians( pitch );
+    float r_yaw     = degrees_to_radians( yaw );
+    float cos_pitch = __cosf( r_pitch );
+    float sin_pitch = __sinf( r_pitch );
+    float cos_yaw   = __cosf( r_yaw );
+    float sin_yaw   = __sinf( r_yaw );
 
-	return make_float4(cos_pitch*sin_yaw,
-					   sin_pitch,
-					   cos_pitch*cos_yaw);
+    return make_float4( cos_pitch * sin_yaw, sin_pitch, cos_pitch * cos_yaw );
 }
-
--- raystructs.h
+++ raystructs.h
@@ -1,235 +1,235 @@
-//#pragma once
-///*********************************************************************
-// * @file	raystructs.h
-// * @brief	Structs for raytracing
-// *			3D vectors, spheres, lights.... 
-// * 
-// * @author	Johan Karlsson - github.com/dosalock
-// * @date	November 2024
-// *********************************************************************/
-//
-//
-//
-///*-----------------------------Includes------------------------------*/
-//#define _USE_MATH_DEFINES
-//
-//#include <cmath>
-//#include "Windows.h"
-//#include <cuda_runtime.h>
-//#include <cuda_device_runtime_api.h>
-///*-----------------------------Structs-------------------------------*/
-//
-///**
-// * @struct Vect3D raystruct.h
-// * @brief Three dimensional vector
-// * 
-// */
-////struct Vect3D 
-////{
-////	double x; // @brief Represents the vector's position along the X-axis
-////	double y; // @brief Represents the vector's position along the Y-axis
-////	double z; // @brief Represents the vector's position along the Z-axis
-////
-////	Vect3D(double x = 0, double y = 0, double z = 0) : x(x), y(y), z(z) { }
-////	
-////	__host__ __device__ Vect3D operator-(const Vect3D &other) const { return Vect3D(x - other.x, y - other.y, z - other.z); }
-////	__host__ __device__ Vect3D operator+(const Vect3D &other) const { return Vect3D(x + other.x, y + other.y, z + other.z); }
-////	__host__ __device__ Vect3D operator*(const Vect3D &other) const { return Vect3D(x * other.x, y * other.y, z * other.z); }
-////	__host__ __device__ Vect3D operator/(const Vect3D &other) const { return Vect3D(x / other.x, y / other.y, z / other.z); }
-////	__host__ __device__ Vect3D operator/(const double &other) const { return Vect3D(x / other, y / other, z / other); }
-////	__host__ __device__ Vect3D operator*(const double &other) const { return Vect3D(x * other, y * other, z * other); }
-////	__host__ __device__ Vect3D cross(const Vect3D &other) const { return Vect3D(y * other.z - z * other.y, z*other.x - x*other.z, x*other.y - y*other.z); }
-////	__host__ __device__ Vect3D invert() const { return Vect3D(x * -1, y * -1, z * -1); }
-////	__host__ __device__ double len() const { return rsqrtf(x * x + y * y + z * z); }
-////	__host__ __device__ Vect3D norm() const { return Vect3D(x, y, z) / len(); }
-////	__host__ __device__ double dot(const Vect3D &other) const { return x * other.x + y * other.y + z * other.z; }
-////};
-//
-//
-///**
-// * @struct Sphere raystruct.h
-// * @brief Sphere with position, radius, color, and specularity
-// */
-//struct Sphere
+#pragma once
+/*********************************************************************
+ * @file	raystructs.h
+ * @brief	Structs for raytracing
+ *			3D vectors, spheres, lights.... 
+ * 
+ * @author	Johan Karlsson - github.com/dosalock
+ * @date	November 2024
+ *********************************************************************/
+
+
+
+/*-----------------------------Includes------------------------------*/
+#define _USE_MATH_DEFINES
+
+#include <cmath>
+#include "Windows.h"
+#include <cuda_runtime.h>
+#include <cuda_device_runtime_api.h>
+/*-----------------------------Structs-------------------------------*/
+
+/**
+ * @struct Vect3D raystruct.h
+ * @brief Three dimensional vector
+ * 
+ */
+//struct Vect3D 
 //{
-//	Vect3D center;
-//	double radius;
-//	COLORREF color;
-//	int specularity;
-//	double reflective;
-//	double sRadius;
+//	double x; // @brief Represents the vector's position along the X-axis
+//	double y; // @brief Represents the vector's position along the Y-axis
+//	double z; // @brief Represents the vector's position along the Z-axis
 //
-//	Sphere(
-//		Vect3D center = {},
-//		double radius = 0,
-//		COLORREF color = RGB(0, 0, 0),
-//		int specularity = 0,
-//		double reflective = 0)
-//		:
-//		center(center),
-//		radius(radius),
-//		color(color),
-//		specularity(specularity),
-//		reflective(reflective),
-//		sRadius(radius*radius){}
+//	Vect3D(double x = 0, double y = 0, double z = 0) : x(x), y(y), z(z) { }
+//	
+//	__host__ __device__ Vect3D operator-(const Vect3D &other) const { return Vect3D(x - other.x, y - other.y, z - other.z); }
+//	__host__ __device__ Vect3D operator+(const Vect3D &other) const { return Vect3D(x + other.x, y + other.y, z + other.z); }
+//	__host__ __device__ Vect3D operator*(const Vect3D &other) const { return Vect3D(x * other.x, y * other.y, z * other.z); }
+//	__host__ __device__ Vect3D operator/(const Vect3D &other) const { return Vect3D(x / other.x, y / other.y, z / other.z); }
+//	__host__ __device__ Vect3D operator/(const double &other) const { return Vect3D(x / other, y / other, z / other); }
+//	__host__ __device__ Vect3D operator*(const double &other) const { return Vect3D(x * other, y * other, z * other); }
+//	__host__ __device__ Vect3D cross(const Vect3D &other) const { return Vect3D(y * other.z - z * other.y, z*other.x - x*other.z, x*other.y - y*other.z); }
+//	__host__ __device__ Vect3D invert() const { return Vect3D(x * -1, y * -1, z * -1); }
+//	__host__ __device__ double len() const { return rsqrtf(x * x + y * y + z * z); }
+//	__host__ __device__ Vect3D norm() const { return Vect3D(x, y, z) / len(); }
+//	__host__ __device__ double dot(const Vect3D &other) const { return x * other.x + y * other.y + z * other.z; }
 //};
-//
-//
-///**
-// * @struct QuadraticRoots raystruct.h
-// * @brief Simple two double struct to return answer from the quadratic formula
-// */
-//struct QuadraticRoots
-//{
-//	double t1;
-//	double t2;
-//	QuadraticRoots(double t1 = 0, double t2 = 0) : t1(t1), t2(t2) {}
-//};
-//
-//
-///**
-// * @struct Light raystruct.h
-// * @brief Light for rendering, different modes - DIRECTIONAL, POINT, and AMBIENT
-// */
-//struct Light 
-//{
-//	enum LightType { DIRECTIONAL, POINT, AMBIENT }  type;
-//	double intensity;
-//	Vect3D pos;
-//};
-//
-//struct Camera
-//{
-//	Vect3D position;
-//	double yaw;
-//	double pitch;
-//	double roll;
-//
-//	double DegreesToRadian(double degrees)
-//	{
-//		return degrees * M_PI / 180.0;
-//	}
-//
-//	/**
-//	 * @brief Rotate around Y-axis (left-right rotation)
-//	 * @param[in] direction - D returned by CanvasToViewPort()
-//	 * @param[in] yaw - Degrees to rotate 
-//	 * @return Rotated vector
-//	 */
-//	Vect3D RotateYaw(Vect3D direction, double yaw)
-//	{
-//		double rad = DegreesToRadian(yaw);
-//		double cosY = cos(rad);
-//		double sinY = sin(rad);
-//		
-//		return Vect3D(
-//			direction.x * cosY + direction.z * sinY,
-//			direction.y,
-//			-direction.x * sinY + direction.z * cosY
-//		);
-//	}
-//
-//	/**
-//	 * @brief Rotate around X-axis (up-down rotation)
-//	 * @param direction - D returned by CanvasToViewPort()
-//	 * @param pitch - Degrees to rotate
-//	 * @return Rotated vector
-//	 */
-//	Vect3D RotatePitch(Vect3D direction, double pitch)
-//	{
-//		
-//		double rad = DegreesToRadian(pitch);
-//		double cosX = cos(rad);
-//		double sinX = sin(rad);
-//
-//		return Vect3D(
-//			direction.x,
-//			direction.y * cosX - direction.z * sinX,
-//			direction.y * sinX + direction.z * cosX
-//		);
-//	}
-//
-//	/**
-//	 * @brief Rotate around Z-axis (side-side rotation)
-//	 * @param direction - D returned by CanvasToViewPort()
-//	 * @param roll - Degrees to rotate
-//	 * @return Rotated Vector
-//	 */
-//	Vect3D RotateRoll(Vect3D direction, double roll)
-//	{
-//
-//		double rad = DegreesToRadian(roll);
-//		double cosZ = cos(rad);
-//		double sinZ = sin(rad);
-//		
-//		return Vect3D(
-//			direction.x * cosZ - direction.y * sinZ,
-//			direction.x * sinZ + direction.y * cosZ,
-//			direction.z
-//		);
-//	}
-//
-//	Vect3D ApplyCameraRotation(Vect3D direction, Camera cam)
-//	{
-//		direction = RotateYaw(direction, cam.yaw);
-//		direction = RotatePitch(direction, cam.pitch);
-//		direction = RotateRoll(direction, cam.roll);
-//
-//		return direction;
-//
-//	}
-//
-//
-//	/**
-//	 * @brief Calculates normalized vector with forward direction for use in " W = move forward "
-//	 * @return Normalized vector
-//	 */
-//	Vect3D CalculateForwardFromEuler()
-//	{	double rPitch = DegreesToRadian(pitch);
-//		double rYaw = DegreesToRadian(yaw);
-//		float cosPitch = cos(rPitch);
-//		float sinPitch = sin(rPitch);
-//		float cosYaw = cos(rYaw);
-//		float sinYaw = sin(rYaw);
-//
-//		return Vect3D(cosPitch * sinYaw, sinPitch, cosPitch * cosYaw).norm();
-//	}
-//	/**
-//	 * @brief Moves camera forward
-//	 * @param moveSpeed - Movement multiplier, backwards < 0 < forewards
-//	 */
-//	void MoveForward(double moveSpeed)
-//	{
-//		Vect3D forward = CalculateForwardFromEuler();
-//		position.x += forward.x * moveSpeed;
-//		position.y += forward.y * moveSpeed;
-//		position.z += forward.z * moveSpeed;
-//	}
-//	/**
-//	 * @brief Moves camera sideways
-//	 * @param moveSpeed - Movemet multiplier, right < 0 < left
-//	 */
-//	void MoveSideways(double moveSpeed)
-//	{
-//		Vect3D right = CalculateForwardFromEuler().cross(Vect3D(0,1,0));
-//		position.x += right.x * moveSpeed;
-//		position.y += right.y * moveSpeed;
-//		position.z += right.z * moveSpeed;
-//	}
-//
-//};
-///**
-// * @struct Intersection raystruct.h
-// * @brief Pointer to sphere and number T, signifies an intersection between a sphere and a vector */
-//struct Intersection 
-//{
-//	Sphere *closest_sphere;
-//	double closest_t;
-//
-//	Intersection(Sphere *closest_sphere = NULL, 
-//				 double closest_t = INFINITY)
-//		         :
-//		         closest_sphere(closest_sphere),
-//		         closest_t(closest_t) {}
-//};
-//
+
+
+/**
+ * @struct Sphere raystruct.h
+ * @brief Sphere with position, radius, color, and specularity
+ */
+struct Sphere
+{
+	Vect3D center;
+	double radius;
+	COLORREF color;
+	int specularity;
+	double reflective;
+	double sRadius;
+
+	Sphere(
+		Vect3D center = {},
+		double radius = 0,
+		COLORREF color = RGB(0, 0, 0),
+		int specularity = 0,
+		double reflective = 0)
+		:
+		center(center),
+		radius(radius),
+		color(color),
+		specularity(specularity),
+		reflective(reflective),
+		sRadius(radius*radius){}
+};
+
+
+/**
+ * @struct QuadraticRoots raystruct.h
+ * @brief Simple two double struct to return answer from the quadratic formula
+ */
+struct QuadraticRoots
+{
+	double t1;
+	double t2;
+	QuadraticRoots(double t1 = 0, double t2 = 0) : t1(t1), t2(t2) {}
+};
+
+
+/**
+ * @struct Light raystruct.h
+ * @brief Light for rendering, different modes - DIRECTIONAL, POINT, and AMBIENT
+ */
+struct Light 
+{
+	enum LightType { DIRECTIONAL, POINT, AMBIENT }  type;
+	double intensity;
+	Vect3D pos;
+};
+
+struct Camera
+{
+	Vect3D position;
+	double yaw;
+	double pitch;
+	double roll;
+
+	double DegreesToRadian(double degrees)
+	{
+		return degrees * M_PI / 180.0;
+	}
+
+	/**
+	 * @brief Rotate around Y-axis (left-right rotation)
+	 * @param[in] direction - D returned by CanvasToViewPort()
+	 * @param[in] yaw - Degrees to rotate 
+	 * @return Rotated vector
+	 */
+	Vect3D RotateYaw(Vect3D direction, double yaw)
+	{
+		double rad = DegreesToRadian(yaw);
+		double cosY = cos(rad);
+		double sinY = sin(rad);
+		
+		return Vect3D(
+			direction.x * cosY + direction.z * sinY,
+			direction.y,
+			-direction.x * sinY + direction.z * cosY
+		);
+	}
+
+	/**
+	 * @brief Rotate around X-axis (up-down rotation)
+	 * @param direction - D returned by CanvasToViewPort()
+	 * @param pitch - Degrees to rotate
+	 * @return Rotated vector
+	 */
+	Vect3D RotatePitch(Vect3D direction, double pitch)
+	{
+		
+		double rad = DegreesToRadian(pitch);(
+		double cosX = cos(rad);             (
+		double sinX = sin(rad);             (
+
+		return Vect3D(
+			direction.x,
+			direction.y * cosX - direction.z * sinX,
+			direction.y * sinX + direction.z * cosX
+		);
+	}
+
+	/**
+	 * @brief Rotate around Z-axis (side-side rotation)
+	 * @param direction - D returned by CanvasToViewPort()
+	 * @param roll - Degrees to rotate
+	 * @return Rotated Vector
+	 */
+	Vect3D RotateRoll(Vect3D direction, double roll)
+	{
+
+		double rad = DegreesToRadian(roll);
+		double cosZ = cos(rad);
+		double sinZ = sin(rad);
+		
+		return Vect3D(
+			direction.x * cosZ - direction.y * sinZ,
+			direction.x * sinZ + direction.y * cosZ,
+			direction.z
+		);
+	}
+
+	Vect3D ApplyCameraRotation(Vect3D direction, Camera cam)
+	{
+		direction = RotateYaw(direction, cam.yaw);
+		direction = RotatePitch(direction, cam.pitch);
+		direction = RotateRoll(direction, cam.roll);
+
+		return direction;
+
+	}
+
+
+	/**
+	 * @brief Calculates normalized vector with forward direction for use in " W = move forward "
+	 * @return Normalized vector
+	 */
+	Vect3D CalculateForwardFromEuler()
+	{	double rPitch = DegreesToRadian(pitch);
+		double rYaw = DegreesToRadian(yaw);
+		float cosPitch = cos(rPitch);
+		float sinPitch = sin(rPitch);
+		float cosYaw = cos(rYaw);
+		float sinYaw = sin(rYaw);
+
+		return Vect3D(cosPitch * sinYaw, sinPitch, cosPitch * cosYaw).norm();
+	}
+	/**
+	 * @brief Moves camera forward
+	 * @param moveSpeed - Movement multiplier, backwards < 0 < forewards
+	 */
+	void MoveForward(double moveSpeed)
+	{
+		Vect3D forward = CalculateForwardFromEuler();
+		position.x += forward.x * moveSpeed;
+		position.y += forward.y * moveSpeed;
+		position.z += forward.z * moveSpeed;
+	}
+	/**
+	 * @brief Moves camera sideways
+	 * @param moveSpeed - Movemet multiplier, right < 0 < left
+	 */
+	void MoveSideways(double moveSpeed)
+	{
+		Vect3D right = CalculateForwardFromEuler().cross(Vect3D(0,1,0));
+		position.x += right.x * moveSpeed;
+		position.y += right.y * moveSpeed;
+		position.z += right.z * moveSpeed;
+	}
+
+};
+/**
+ * @struct Intersection raystruct.h
+ * @brief Pointer to sphere and number T, signifies an intersection between a sphere and a vector */
+struct Intersection 
+{
+	Sphere *closest_sphere;
+	double closest_t;
+
+	Intersection(Sphere *closest_sphere = NULL, 
+				 double closest_t = INFINITY)
+		         :
+		         closest_sphere(closest_sphere),
+		         closest_t(closest_t) {}
+};
+
